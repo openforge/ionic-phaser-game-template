965d127941c8cf0b70a3077291ba86bbf6315066
Jedidiah Weller <jedi@openforge.io>
2022-02-07 14:42:19 -0500

    feat(): adding shop component page and hooking up navigation with ion-item list

--- apps/example-app/src/app/app.module.ts
+++ apps/example-app/src/app/app.module.ts
@@ -7,9 +7,10 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { PhaserSingletonService } from 'libs/example-app/phaser/singleton/src';
+import { ShopPageComponent } from './shop/shop.component';
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [AppComponent, ShopPageComponent],
     entryComponents: [],
     imports: [BrowserModule, IonicModule.forRoot(), PhaserSingletonService.forRoot(), AppRoutingModule],
     providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
---
+++ apps/example-app/src/app/shop/shop.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'openforge-shop',
+  templateUrl: './shop.component.html',
+  styleUrls: ['./shop.component.scss']
+})
+export class ShopPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
