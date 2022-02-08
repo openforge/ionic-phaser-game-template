f95809a5db8a014125d02661e58368d21eeae413
Jedidiah Weller <jedi@openforge.io>
2022-02-08 15:55:58 -0500

    feat(): in-progress for adding the background image. need to debug why asset is not loading correctly

--- libs/example-app/phaser/singleton/src/lib/scenes/forge.scene.ts
+++ libs/example-app/phaser/singleton/src/lib/scenes/forge.scene.ts
@@ -1,13 +1,17 @@
 import * as Phaser from 'phaser';
 
 export class ForgeScene extends Phaser.Scene {
+    private backgroundKey = 'background-image'; // * Store the background image name
+
     constructor() {
         super({ key: 'preloader' });
     }
 
     preload() {
         try {
-            // this.load.image('background-image', 'https://cdn1.epicgames.com/ue/product/Screenshot/04-1920x1080-d82f40524f9296f71a156b5e37241e11.jpg?resize=1&w=1920');
+            console.warn('Loading image');
+            this.load.image(this.backgroundKey, './assets/blank_coworking.png');
+            // this.load.svg(this.backgroundKey, this.backgroundKey, { scale: 1 });
         } catch (e) {
             console.error('preloader.scene.ts', 'error preloading', e);
         }
@@ -17,12 +21,13 @@ export class ForgeScene extends Phaser.Scene {
      * * Phaser will only call create after all assets in Preload have been loaded
      */
     create() {
+        console.warn('Creating scene image');
         // eslint-disable-next-line no-magic-numbers
         this.add.text(20, 20, 'Playing game!');
-        // const image = this.add.image(this.cameras.main.width / 2, this.cameras.main.height / 2, 'background-image');
-        // const scaleX = this.cameras.main.width / image.width;
-        // const scaleY = this.cameras.main.height / image.height;
-        // const scale = Math.max(scaleX, scaleY);
-        // image.setScale(scale).setScrollFactor(0);
+        const backgroundImage = this.add.image(this.cameras.main.width, this.cameras.main.height, this.backgroundKey);
+        const scaleX = this.cameras.main.width / backgroundImage.width;
+        const scaleY = this.cameras.main.height / backgroundImage.height;
+        const scale = Math.max(scaleX, scaleY);
+        backgroundImage.setScale(scale).setScrollFactor(0);
     }
 }
