d8d58cb861979205b7ed2dbc3651b09f2d6cd00b
Jedidiah Weller <jedi@openforge.io>
2022-02-08 08:21:51 -0500

    feat(): adding style lint to the project and updating import methods

--- apps/example-app/src/app/app-routing.module.ts
+++ apps/example-app/src/app/app-routing.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { HomePage } from './home/home.page';
+
+import { HomePageComponent } from './home/home.page';
 import { ShopPageComponent } from './shop/shop.component';
 
 const routes: Routes = [
     {
         path: 'home',
-        component: HomePage
+        component: HomePageComponent,
     },
     {
         path: 'shop',
-        component: ShopPageComponent
+        component: ShopPageComponent,
     },
     {
         path: '',
