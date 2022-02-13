276a04fad9dc64916c95eac1f55d6c8313762aab
Ricardo Madrid <ricardomadrid385@gmail.com>
2022-02-12 18:21:50 -0600

    Blacksmith class is now instantiated, moved preload of character into its own function

--- libs/shared/data-access-model/src/lib/blacksmith/blacksmith.class.ts
+++ libs/shared/data-access-model/src/lib/blacksmith/blacksmith.class.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-magic-numbers */
-/* eslint no-console: ["error", { allow: ["trace"] }] */
+/* eslint no-console: ["error", { allow: ["trace", "error"] }] */
 /* eslint no-restricted-syntax: ["error", "BinaryExpression[operator='in']"] */
 import * as Phaser from 'phaser';
 
@@ -8,9 +8,25 @@ import { Human } from '../human/human.class';
 export class Blacksmith extends Phaser.GameObjects.Sprite implements Human {
     constructor(phaserScene: Phaser.Scene, textureKey: string) {
         super(phaserScene, 400, 484, textureKey);
-        console.trace('Data access model', 'blacksmith.class', 'constructor()', textureKey);
         this.scene.add.existing(this);
         this.setVisible(true);
         this.play(textureKey);
+    }
+
+    /**
+     * * Builds respective class asynchronously
+     *
+     * @param phaserScene
+     * @param textureKey
+     * @returns Promise<Blacksmith>
+     */
+    public static async build(phaserScene: Phaser.Scene, textureKey: string): Promise<Blacksmith> {
+        console.trace('Data access model', 'blacksmith.class', 'constructor()');
+        const tempObject = new Blacksmith(phaserScene, textureKey);
+        try {
+            return tempObject;
+        } catch (e) {
+            console.error('Error creating blacksmith');
+        }
     }
 }
