82eb370354e4133135454d3011ff0b8c9f616466
Jedidiah Weller <jedi@openforge.io>
2022-04-27 13:01:07 -0400

    feat(): adding start of composition pattern

---
+++ libs/shared/data-access-model/src/lib/stats/final-bonus.class.ts
@@ -0,0 +1,11 @@
+/**
+ * * RawBonus is...
+ */
+
+import { BaseAttribute } from './base-attribute.class';
+
+export class FinalBonus extends BaseAttribute {
+    constructor(value, multiplier) {
+        super(value, multiplier);
+    }
+}
---
+++ libs/shared/data-access-model/src/lib/stats/raw-bonus.class.ts
@@ -0,0 +1,11 @@
+/**
+ * * RawBonus is...
+ */
+
+import { BaseAttribute } from './base-attribute.class';
+
+export class RawBonus extends BaseAttribute {
+    constructor(value, multiplier) {
+        super(value, multiplier);
+    }
+}
