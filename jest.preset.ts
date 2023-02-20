90dda31a6b7e5d4f8e4349d43d22f659bd5908ac
Ricardo Madrid <ricardomadrid385@gmail.com>
2023-02-20 11:07:24 -0600

    feat(): nx angular migrated to v15

--- jest.preset.ts
+++ jest.preset.ts
@@ -1,6 +1,8 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-const nxPreset = require('@nrwl/jest/preset');
+import { getJestProjects } from '@nrwl/jest';
 
-module.exports = { ...nxPreset };
+module.exports = {
+    projects: getJestProjects(),
+};
--- libs/shared/data-access-model/jest.config.ts
+++ libs/shared/data-access-model/jest.config.ts
@@ -1,4 +1,5 @@
-module.exports = {
+/* eslint-disable */
+export default {
     displayName: 'data-access-model',
 
     setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
