387c3c35624254e07612dec36f46d2c6c57e450b
Ricardo Madrid <ricardomadrid385@gmail.com>
2023-02-23 21:15:35 -0600

    feat(): added react example app to monorepo, also added eslint custom config

---
+++ apps/example-app-react/src/app/app.tsx
@@ -0,0 +1,16 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.scss';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+    return (
+        <>
+            <NxWelcome title="example-app-react" />
+
+            <div />
+        </>
+    );
+}
+
+export default App;
